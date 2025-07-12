e7b3ed63724a445e37ba2688ee689ef476137286
claudiolpz <claudio.lopez1@mail.udp.cl>
2025-07-12 19:03:29 -0400

    Pasar datos a la vista

--- src/main/java/com/curso/spring/app1/springboot_applications/controllers/EjemploController.java
+++ src/main/java/com/curso/spring/app1/springboot_applications/controllers/EjemploController.java
@@ -1,11 +1,19 @@
 package com.curso.spring.app1.springboot_applications.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class EjemploController {
+    
+    @GetMapping("/detalles_info")
 
-    public String info(){
-        return "Detalles_info";
+    public String info(Model model){
+        model.addAttribute("titulo", "Servidor en Linea");
+        model.addAttribute("Servidor", "Claudio Lopez");
+        model.addAttribute("Magallanes", "El Primer Grande");
+        return "detalles_info";
     }
 }
