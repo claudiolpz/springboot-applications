fa42d0bd46c2e3272ddf07c019d0917bfa9ab083
claudiolpz <claudio.lopez1@mail.udp.cl>
2025-07-13 01:33:49 -0400

    Clase DTO

--- src/main/java/com/curso/spring/app1/springboot_applications/controllers/EjemploRestController.java
+++ src/main/java/com/curso/spring/app1/springboot_applications/controllers/EjemploRestController.java
@@ -1,17 +1,17 @@
 package com.curso.spring.app1.springboot_applications.controllers;
 
-import java.util.HashMap;
-import java.util.Map;
+// import java.util.HashMap;
+// import java.util.Map;
 
-import org.springframework.ui.Model;
+// import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.curso.spring.app1.springboot_applications.models.Empleados;
+// import com.curso.spring.app1.springboot_applications.models.Empleados;
+import com.curso.spring.app1.springboot_applications.models.dto.ClaseDTO;
 
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
@@ -20,15 +20,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class EjemploRestController {
     
     @GetMapping(path="/detalles_info2")
-    // @RequestMapping(path="/detalles_info2", method=RequestMethod.GET)
 
-    public Map<String, Object> detalles_info2() {
-        Empleados empleado1 = new Empleados("Chester","Cortes", "San Bernarbeu", "Gerente", 35, 123456789, 001);
+    // public Map<String, Object> detalles_info2() {
+    //     Empleados empleado1 = new Empleados("Chester","Cortes", "San Bernarbeu", "Gerente", 35, 123456789, 001);
 
+    //     Map<String, Object> respuesta = new HashMap<>();
+    //     respuesta.put("Empleado", "Datos del Empleado");
+    //     respuesta.put("Información", empleado1);
+    //     return respuesta;
+    // }
 
-        Map<String, Object> respuesta = new HashMap<>();
-        respuesta.put("Empleado", "Datos del Empleado");
-        respuesta.put("Información", empleado1);
-        return respuesta;
+     public ClaseDTO detalles_info2() {
+        ClaseDTO usuario1 = new ClaseDTO();
+        usuario1.setTitulo("Administrador");
+        usuario1.setUsuario("Claudio Lopez");
+        return usuario1;
     }
 }
