aeae405a58133e724e984eec332df309c83df223
claudiolpz <claudio.lopez1@mail.udp.cl>
2025-07-13 01:04:50 -0400

    Objeto Model

--- src/main/java/com/curso/spring/app1/springboot_applications/controllers/EjemploController.java
+++ src/main/java/com/curso/spring/app1/springboot_applications/controllers/EjemploController.java
@@ -1,10 +1,11 @@
 package com.curso.spring.app1.springboot_applications.controllers;
 
-import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.curso.spring.app1.springboot_applications.models.Empleados;
 
 
 @Controller
@@ -12,10 +13,10 @@ public class EjemploController {
     
     @GetMapping("/detalles_info")
 
-    public String info(Map<String, Object> modelo) {
-        modelo.put("titulo", "Servidor en Linea");
-        modelo.put("Servidor", "Claudio Lopez");
-        modelo.put("Magallanes", "El Primer Grande");
+    public String info(Model model) {
+        
+        Empleados empleado1 = new Empleados("Chester","Cortes", "San Bernarbeu", "Gerente", 35, 123456789, 001);
+        model.addAttribute("Empleado", empleado1);
         return "detalles_info";
     }
 }
--- src/main/java/com/curso/spring/app1/springboot_applications/controllers/EjemploRestController.java
+++ src/main/java/com/curso/spring/app1/springboot_applications/controllers/EjemploRestController.java
@@ -5,19 +5,30 @@ import java.util.Map;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.curso.spring.app1.springboot_applications.models.Empleados;
+
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
+@RequestMapping("/api")
 public class EjemploRestController {
     
-    @GetMapping("/detalles_info2")
+    @GetMapping(path="/detalles_info2")
+    // @RequestMapping(path="/detalles_info2", method=RequestMethod.GET)
 
     public Map<String, Object> detalles_info2() {
+        Empleados empleado1 = new Empleados("Chester","Cortes", "San Bernarbeu", "Gerente", 35, 123456789, 001);
+
+
         Map<String, Object> respuesta = new HashMap<>();
-        respuesta.put("titulo", "Servidor en Linea");
-        respuesta.put("Servidor", "Claudio Lopez");
-        respuesta.put("Magallanes", "El Primer Grande");
+        respuesta.put("Empleado", "Datos del Empleado");
+        respuesta.put("Información", empleado1);
         return respuesta;
     }
 }
